5ef9889255921bf8762be210216bdecce7ecbdb5
ChrisHuntsinger <huntsingerchristian@gmail.com>
2025-03-19 14:03:08 -0500

    Acceptable for now

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function toggleMenu()
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function sendMail()
+{
+    let params =
+    {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    }
+
+    emailjs.send("service_6v4e9ch", "template_q7cadxv", params).then(alert("Your message has been sent!"))
+}
